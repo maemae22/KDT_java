6396b11e3016c6deda258cd1389e59a4145ffca9
maemae22 <maemae2@naver.com>
2022-09-20 17:23:18 +0900

    220920 온라인강의 Practice

---
+++ java/src/me/java/online/Gugudan.java
@@ -0,0 +1,15 @@
+package me.java.online;
+
+public class Gugudan {
+
+    public static void main(String[] args) {
+
+        for ( int dan=2; dan<=9 ; dan++) {
+            for ( int count=1; count<=9 ; count ++) {
+                System.out.println(dan+"x"+count+"="+dan*count);
+            }
+
+            System.out.println();
+        }
+    }
+}

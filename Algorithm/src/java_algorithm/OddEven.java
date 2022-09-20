03bd187d93e30841f56b887734e1ea6d4d819e4a
maemae22 <maemae2@naver.com>
2022-09-20 19:11:21 +0900

    220920 알고리즘 문제풀이

---
+++ Algorithm/src/java_algorithm/OddEven.java
@@ -0,0 +1,13 @@
+package java_algorithm;
+
+class OddEven {
+    public String solution(int num) {
+
+        if (num%2==0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+
+    }
+}

e7aad7a73aac28bf1f984de9e7f1869c26cc1121
maemae22 <maemae2@naver.com>
2022-09-20 17:15:57 +0900

    220919 수업 Practice

---
+++ java/src/me/java/practice/Prac3.java
@@ -0,0 +1,10 @@
+package me.java.practice;
+
+public class Prac3 {
+    public static void main(String[] args) {
+        int a = 10 ;
+        System.out.println(a); // 변수 (정수 데이터를 담고 있는 변수)
+        System.out.println('a'); // 리터럴 (문자 리터럴)
+        System.out.println("a"); // 리터럴 (문자열 리터럴)
+    }
+}
